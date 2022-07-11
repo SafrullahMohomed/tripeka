c02a776a44eb5c2b12ae24c76723d35e12c2288b
mohamedms <emails3msm@gmail.com>
2022-07-11 14:33:08 +0530

    first commit with basic folder structures used react for front-end and spring boot for back-end

---
+++ springboot/src/main/java/com/tripeka/tripeka/TripekaApplication.java
@@ -0,0 +1,13 @@
+package com.tripeka.tripeka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripekaApplication.class, args);
+	}
+
+}
---
+++ springboot/src/test/java/com/tripeka/tripeka/TripekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripeka.tripeka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
