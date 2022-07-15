52c825dbcc95eb352b5125798394c3f30bb545b1
AbdulQadir98 <abdul.qadirv98@gmail.com>
2022-07-15 20:12:07 +0530

    change dashboard

--- react/vili/src/components/Dashboard.js
+++ react/vili/src/components/Dashboard.js
@@ -1,12 +1,25 @@
 import img1 from '../assets/arugam.jpg'
 import img2 from '../assets/dalada.jpg'
 import img3 from '../assets/jaffna.jpg'
+import group from '../assets/group.png'
 
 const Dashboard = () => {
    return ( 
       <div class="absolute right-72 left-60 h-full bg-gray-200 px-8 py-4">
-         <div class="w-full h-16 mb-7 rounded-lg bg-gray-100"></div>
-         <div class="flex justify-evenly w-full h-48 mb-7">
+         <div class="flex w-full h-16 mb-7 rounded-lg bg-gray-100">
+            <div class="w-3/12 h-full p-3 text-lg bg-gray-300">Hi, Abdul!</div>
+            <div class="w-3/12 h-full p-3 bg-gray-200"></div>
+            <div class="w-5/12 h-full p-3 bg-gray-100">
+               
+            </div>
+            <div class="w-1/12 h-full p-3 bg-gray-300">
+               <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+               </svg>
+            </div>
+         </div>
+
+         <div class="flex justify-evenly w-full h-48 mb-7 p-2">
             <div class="h-full w-56 rounded-lg bg-gray-300">
                <img src={img1} class="w-full h-full rounded" alt="" />
             </div>
@@ -21,7 +34,7 @@ const Dashboard = () => {
          <div class="flex w-full h-72 mb-7 rounded-lg bg-white">
 
             <div class="w-3/4 h-full px-4 py-3 bg-gray-200">
-               <div className="flex flex-col w-full h-full overflow-hidden rounded-lg bg-gray-300">
+               <div className="flex flex-col w-full h-full overflow-auto rounded-lg bg-gray-300 scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-100">
                   <div class="pt-3 pl-6 mb-1 font-semibold">
                      Popular Destinations
                   </div>
@@ -41,6 +54,9 @@ const Dashboard = () => {
                            <span class="w-16 h-full bg-gray-400">
                               <img src={img3} class="w-full h-full rounded-lg" alt="" />
                            </span>
+                           <div className="ml-4">
+                              Galle Fort<br />Galle
+                           </div>
                         </a>
                      </li>
                      <li class="px-5 py-3">
@@ -48,6 +64,9 @@ const Dashboard = () => {
                            <span class="w-16 h-full bg-gray-400">
                               <img src={img2} class="w-full h-full rounded-lg" alt="" />
                            </span>
+                           <div className="ml-4">
+                              Galle Fort<br />Galle
+                           </div>
                         </a>
                      </li>
                   </ul>
@@ -56,9 +75,16 @@ const Dashboard = () => {
 
             <div class="w-1/4 h-full bg-gray-200 p-3">
                <div class="w-full h-full rounded-lg p-2 bg-gray-300">
-                  <div class="w-full h-3/4 bg-gray-100"></div>
-                  <div class="w-full h-1/4 bg-gray-200 text-center">
-                     <button class=" mt-4 px-2 py-1">Create Group</button>
+                  <div class="align-middle w-full h-3/4 bg-white">
+                     <img src={group} class="pt-4" alt="" />
+                  </div>
+                  <div class="w-full h-1/4 pt-4 bg-gray-200"><center>
+                     <button class="flex m-0 px-2 py-1 bg-emerald-300 border-transparent">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mt-0.5" viewBox="0 0 20 20" fill="currentColor">
+                           <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clip-rule="evenodd" />
+                        </svg>
+                        <div class="pl-1">Create Group</div>
+                     </button></center>
                   </div>
                </div>
             </div>
--- react/vili/src/components/Panel.js
+++ react/vili/src/components/Panel.js
@@ -1,4 +1,7 @@
 import img from '../assets/customer2.jpg'
+import img1 from '../assets/arugam.jpg'
+import img2 from '../assets/dalada.jpg'
+import img3 from '../assets/jaffna.jpg'
 
 const Panel = () => {
    return ( 
@@ -31,9 +34,36 @@ const Panel = () => {
             </div>
          </div>
 
-         <div class="w-full h-48 mb-7 rounded-lg bg-gray-300"><center><br />Calender</center></div>
+         <div class="w-full h-48 mb-10 rounded-lg bg-gray-300"><center><br />Calender</center></div>
          
-         <div class="w-full h-72 mb-7 rounded-lg bg-white">
+         <div class="w-full h-64 mb-7 rounded-lg bg-white">
+         <div className="flex flex-col w-full h-full overflow-auto rounded-lg bg-gray-300 scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-100">
+                  <div class="pt-3 pl-6 mb-1 font-semibold">
+                     My trips
+                  </div>
+                  <ul>
+                     <li class="p-2 pl-4">
+                        <a href="#" class="flex h-16 w-11/12 items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white bg-white dark:hover:bg-gray-700">
+                           <span class="w-16 h-full bg-gray-400">
+                              <img src={img1} class="w-full h-full rounded-lg" alt="" />
+                           </span>
+                           <div className="ml-4">
+                              Galle Fort<br />Galle
+                           </div>
+                        </a>
+                     </li>
+                     <li class="p-2 pl-4">
+                        <a href="#" class="flex h-16 w-11/12 items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white bg-white dark:hover:bg-gray-700">
+                           <span class="w-16 h-full bg-gray-400">
+                              <img src={img3} class="w-full h-full rounded-lg" alt="" />
+                           </span>
+                           <div className="ml-4">
+                              Galle Fort<br />Galle
+                           </div>
+                        </a>
+                     </li>
+                  </ul>
+               </div>
          </div>
       </div>
    );
