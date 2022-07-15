3fed034621a6244ce91cf28a37990f33d3282b7c
AbdulQadir98 <abdul.qadirv98@gmail.com>
2022-07-15 23:35:25 +0530

    Routing

--- react/vili/src/components/Dashboard.js
+++ react/vili/src/components/Dashboard.js
@@ -10,7 +10,7 @@ const Dashboard = () => {
             <div class="w-3/12 h-full p-3 text-lg bg-gray-200">Hi, Abdul!</div>
             <div class="w-3/12 h-full p-3 bg-gray-200"></div>
             <div class="w-5/12 h-full p-1 bg-gray-200">
-               <form className="max-w-sm px-4">
+               <form className="max-w-sm px-4 drop-shadow-lg">
                   <div className="relative">
                      <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -34,10 +34,12 @@ const Dashboard = () => {
                   </div>
                </form>
             </div>
-            <div class="w-1/12 h-full p-3 bg-gray-200">
-               <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7 bg-white rounded-full" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-               </svg>
+            <div class="w-1/12 h-full p-1 bg-gray-200">
+               <center><div class="w-11 h-11 p-2 rounded-full bg-gray-300">  
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7 bg-white rounded-full" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                     <path stroke-linecap="round" stroke-linejoin="round" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+                  </svg>
+               </div></center>
             </div>
          </div>
 
