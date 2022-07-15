548794da91998a012b29364795d538b59d1b18be
AbdulQadir98 <abdul.qadirv98@gmail.com>
2022-07-15 21:07:02 +0530

    add search

--- react/vili/src/components/Dashboard.js
+++ react/vili/src/components/Dashboard.js
@@ -7,19 +7,41 @@ const Dashboard = () => {
    return ( 
       <div class="absolute right-72 left-60 h-full bg-gray-200 px-8 py-4">
          <div class="flex w-full h-16 mb-7 rounded-lg bg-gray-100">
-            <div class="w-3/12 h-full p-3 text-lg bg-gray-300">Hi, Abdul!</div>
+            <div class="w-3/12 h-full p-3 text-lg bg-gray-200">Hi, Abdul!</div>
             <div class="w-3/12 h-full p-3 bg-gray-200"></div>
-            <div class="w-5/12 h-full p-3 bg-gray-100">
-               
+            <div class="w-5/12 h-full p-1 bg-gray-200">
+               <form className="max-w-sm px-4">
+                  <div className="relative">
+                     <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 left-3"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                     >
+                        <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth={2}
+                              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                        />
+                     </svg>
+                     <input
+                        type="text"
+                        placeholder="Search"
+                        className="w-full h-11 py-3 pl-12 pr-4 text-gray-500 border rounded-full outline-none bg-gray-50 focus:bg-white focus:border-indigo-600"
+                     />
+                  </div>
+               </form>
             </div>
-            <div class="w-1/12 h-full p-3 bg-gray-300">
-               <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <div class="w-1/12 h-full p-3 bg-gray-200">
+               <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7 bg-white rounded-full" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
                </svg>
             </div>
          </div>
 
-         <div class="flex justify-evenly w-full h-48 mb-7 p-2">
+         <div class="flex justify-evenly w-full h-48 mb-7 p-2 bg-gray-100">
             <div class="h-full w-56 rounded-lg bg-gray-300">
                <img src={img1} class="w-full h-full rounded" alt="" />
             </div>
