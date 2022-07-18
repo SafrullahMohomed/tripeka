d2d4aa43b4f0ba6960ff93ac65bfb856649684b4
Ashfaq Ashar <72542210+ashfaq1998-data@users.noreply.github.com>
2022-07-18 14:49:10 +0530

    validation for register and login'

--- react/src/components/Login.js
+++ react/src/components/Login.js
@@ -2,10 +2,17 @@ import React from 'react'
 import Navbar from './Navbar'
 import Footer from './Footer'
 import pop6 from '../assets/loginImage.png'
+import { useForm } from "react-hook-form";
 
 
 
 const Login = () => {
+    const {
+        register,
+        formState: { errors },
+        handleSubmit,
+      } = useForm();
+      const onSubmit = (data) => console.log(data);
   return (
     <div>
         <Navbar/>
@@ -17,7 +24,7 @@ const Login = () => {
                             <img src={pop6} className='w-full' alt="Sample image"/>
                         </div>
                         <div className="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
-                            <form>
+                            <form onSubmit={handleSubmit(onSubmit)}>
                                 <div className="flex flex-row items-center justify-center lg:justify-start">
                                     <p className="text-lg mb-0 mr-4">Sign in with</p>
                                     <button type="button" data-mdb-ripple="true" data-mdb-ripple-color="light" className="inline-block p-3 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out mx-1">
@@ -56,35 +63,59 @@ const Login = () => {
                                 <div class="mb-6">
                                     <input
                                     type="text"
+                                    name = "user"
                                     class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                                     id="exampleFormControlInput2"
+                                    {...register("user", {required: true,pattern: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/i,})}
                                     placeholder="Email address"
                                     />
+                                    <error className = 'text-red-600'>
+                                        {errors.user?.type === "required" && "Email is required"}
+                                        {errors.user?.type === "pattern" &&
+                                        "Entered email is in wrong format"}
+                                    </error>
                                 </div>
 
                                 <div class="mb-6">
                                     <input
                                     type="password"
+                                    name='password'
                                     class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                                     id="exampleFormControlInput2"
+                                    {...register("password", {
+                                        required: true,
+                                        minLength: 5,
+                                        maxLength: 20,
+                                      })}
                                     placeholder="Password"
                                     />
+                                    <error className = 'text-red-600'>
+                                        {errors.password?.type === "minLength" &&
+                                        "Entered password is less than 5 characters"}
+                                        {errors.password?.type === "maxLength" &&
+                                        "Entered password is more than 20 characters"}
+                                    </error>
                                 </div>
 
                                 <div class="flex justify-between items-center mb-6">
                                     <div class="form-group form-check">
                                         <input
                                             type="checkbox"
+                                            name = "check"
                                             class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
                                             id="exampleCheck2"
+                                            {...register("check", { required: true })}
                                         />
                                         <label class="form-check-label inline-block text-gray-800" for="exampleCheck2">Remember me</label>
-                                        
+                                        <br></br>
+                                        <error className = 'text-red-600'>
+                                            {errors.check?.type === "required" && "Please tick it"}
+                                        </error>
                                     </div>
                                     <a href="#!" class="text-gray-800">Forgot password?</a>
                                 </div>
                                 <div class="text-center lg:text-left">
-                                    <button type="button" class="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
+                                    <button type="submit" class="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
                                         Login
                                     </button>
                                     <p class="text-sm font-semibold mt-2 pt-1 mb-0">
