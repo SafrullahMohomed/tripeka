f3468e57f20bafe512f3c03ffc8862c19aa62d57
mohamedms <emails3msm@gmail.com>
2022-07-20 00:06:35 +0530

    resolved merge conflicts

--- react/src/index.js
+++ react/src/index.js
@@ -8,11 +8,6 @@ import Services from './components/Services';
 import Testomonials from './components/Testomonials';
 import Popular from './components/Popular';
 import ContactUs from './components/ContactUs';
-<<<<<<< HEAD
-import Login from './components/Login';
-import Register from './components/Register';
-import Budget from './ms/components/Budget/Budget';
-=======
 import Login from './pages/Login';
 import Register from './pages/Register';
 
@@ -20,7 +15,7 @@ import Sidebar from './components/Sidebar';
 import Panel from './components/Panel';
 import Dashboard from './components/Dashboard';
 import Blogs from './pages/Blogs';
->>>>>>> develop
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -33,13 +28,13 @@ root.render(
       <Route path='/contactus' element={<ContactUs/>}/>
       <Route path='/login' element={<Login/>}/>
       <Route path='/register' element={<Register/>}/>
-<<<<<<< HEAD
+
       <Route path='/ms' element={<Budget/>} />
-=======
+
       
       <Route path='/dashboard' element={<><Sidebar /><Dashboard /><Panel /></>}/>
       <Route path='/blogs' element={<Blogs />}/>
->>>>>>> develop
+
     </Routes>
   </Router>
   
