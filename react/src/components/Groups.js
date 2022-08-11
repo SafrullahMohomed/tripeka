154d52e6fc18fff2340442d3f796017615c04d54
AbdulQadir98 <abdul.qadirv98@gmail.com>
2022-08-12 01:02:14 +0530

    modal

--- react/src/components/Groups.js
+++ react/src/components/Groups.js
@@ -4,10 +4,10 @@ import img2 from '../assets/dalada.jpg'
 import img3 from '../assets/jaffna.jpg'
 import img from '../assets/customer2.jpg'
 import "../styles/Modal.css";
+import TextField from '@mui/material/TextField';
 
 const Groups = () => {
     const [modal, setModal] = useState(false);
-
     const toggleModal = () => {
         setModal(!modal);
     };
@@ -18,27 +18,34 @@ const Groups = () => {
         document.body.classList.remove('active-modal')
     }
     return ( 
+        
         <section class="text-gray-600 body-font mb-10">
             <div class="container px-5 py-5 mx-auto">
                 <div class="w-full mb-8">
-                    <div class="text-xl font-normal title-font mb-4 text-gray-900">My Groups</div>
-                    <button onClick={toggleModal} class="m-0 px-2 py-1 bg-emerald-300 border-transparent text-sm font-normal">
+                    <div class="text-xl font-normal title-font mb-4 text-gray-900"></div>
+                    <button onClick={toggleModal} class="m-0 px-2 py-1 bg-emerald-300 focus:outline-none border-transparent text-sm font-normal">
                         <div class="pl-1">Create Group</div>
                     </button> 
                     {modal && (
                         <div className="modal">
                            <div onClick={toggleModal} className="overlay"></div>
                            <div className="modal-content">
-                              <div className='mb-4 text-xl'>Add friends to 'trip-title'</div>
+                              <div className='mb-4 text-xl'>Create new Group</div>
                               <form>
+                                 <div class="">
+                                    
+                                    <input class="w-full mb-4 border-b-2 border-emerald-400 bg-emerald-100 focus:outline-none text-lg"
+                                        type="text" name="title" required onkeyup="this.setAttribute('value', this.value);" value="" placeholder="Group Name"/>
+                                 </div>
+                                 <div class="mb-4 text-slate-500">Add friends</div>
                                  <div class="inputBox">
                                     <input type="email" name="email" required onkeyup="this.setAttribute('value', this.value);" value="" />
                                     <label>Email</label>
                                  </div>
                                  <div class="mb-2 text-slate-500">People with access</div>
                                  <div class="h-24 overflow-auto scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-100">
-                                    <div class="added">
-                                       <div class='added-names'>
+                                    <div class="modal-added">
+                                       <div class='modal-added-names'>
                                           <div><img src={img} class="h-10 w-10 rounded-full" alt="" /></div>
                                           <div className='ml-3'>Abdul Qadir</div>
                                        </div>
@@ -46,7 +53,7 @@ const Groups = () => {
                                     </div>
                                     
                                  </div>
-                                 <button className="close-modal bg-emerald-300 border-transparent" onClick={toggleModal}>Done</button>
+                                 <button className="float-right m-0 px-4 py-1 bg-emerald-300 focus:outline-none border-transparent text-lg font-normal" onClick={toggleModal}>Done</button>
                               </form>
                            </div>
                         </div> 
@@ -95,6 +102,14 @@ const Groups = () => {
                         <div class="flex-grow">
                             <h2 class="text-gray-900 title-font font-medium">Martin Eden</h2>
                             <p class="text-gray-500">Software Engineer</p>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+                        <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg hover:bg-gray-100 hover:border-transparent">
+                        <div class="w-16 h-16 bg-gray-200 object-cover object-center flex-shrink-0 rounded-full mr-4"></div>
+                        <div class="flex-grow">
+                            <h2 class="text-gray-900 title-font font-normal">No Groups Created</h2>
                         </div>
                         </div>
                     </div>
