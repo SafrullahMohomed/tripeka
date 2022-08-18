9874b81f5a56c51c02d1cdeb649327ebef481784
Ashfaq Ashar <72542210+ashfaq1998-data@users.noreply.github.com>
2022-08-18 22:18:23 +0530

    done email backend

--- springboot/src/main/java/com/example/postgre/Controller/ContactUsController.java
+++ springboot/src/main/java/com/example/postgre/Controller/ContactUsController.java
@@ -6,9 +6,12 @@ import com.example.postgre.Model.Response.ContactUsResponse;
 import com.example.postgre.Model.Response.RegisterResponse;
 import com.example.postgre.repository.ContactUsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.example.postgre.service.EmailSenderService;
 
 import javax.validation.Valid;
 
@@ -20,12 +23,20 @@ public class ContactUsController {
     @Autowired
     private ContactUsRepository contactUsRepository;
 
+    @Autowired
+    private EmailSenderService emailService;
+
     @PostMapping(value = "/contactus")
     public ResponseEntity<ContactUsResponse> ContactUs(@RequestBody @Valid ContactUsRequest contactUsRequest){
 
         try{
             ContactUs contactUs = new ContactUs(contactUsRequest.getName(),contactUsRequest.getEmail(), contactUsRequest.getMessage());
             ContactUs afterSaved = contactUsRepository.save(contactUs);
+
+
+
+            emailService.sendMail(contactUsRequest.getEmail());
+
             return ResponseEntity.status(HttpStatus.OK).body(new ContactUsResponse(true,"Message have been sent Successfully"));
         }catch (Exception e){
             return ResponseEntity.status(HttpStatus.OK).body(new ContactUsResponse(false, e.toString()));
