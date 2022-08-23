e857f899110af58de51cc406fb3a0afeaf893afb
AbdulQadir98 <abdul.qadirv98@gmail.com>
2022-08-23 10:54:35 +0530

    updated group cards

--- react/src/components/GroupModal.js
+++ react/src/components/GroupModal.js
@@ -1,13 +1,5 @@
 import React, { useState } from 'react';
-import Box from '@mui/material/Box';
 import Avatar from '@mui/material/Avatar';
-import SpeedDial from '@mui/material/SpeedDial';
-import SpeedDialIcon from '@mui/material/SpeedDialIcon';
-import SpeedDialAction from '@mui/material/SpeedDialAction';
-import GroupIcon from '@mui/icons-material/Group';
-import CreateIcon from '@mui/icons-material/Create';
-import CallIcon from '@mui/icons-material/Call';
-import HelpCenterIcon from '@mui/icons-material/HelpCenter';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
@@ -19,69 +11,19 @@ import ListItem from '@mui/material/ListItem';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import ListItemText from '@mui/material/ListItemText';
 import PersonIcon from '@mui/icons-material/Person';
-import AddIcon from '@mui/icons-material/Add';
 import { TextField } from "@mui/material";
 import { blue } from '@mui/material/colors';
 
-const actions = [
-    { icon: <GroupIcon />, name: 'Create Group'},
-    { icon: <CreateIcon />, name: 'Review'},
-    { icon: <CallIcon />, name: 'Contact'},
-    { icon: <HelpCenterIcon />, name: 'Help'},
-  ];
-  
 const emails = ['username@gmail.com', 'user02@gmail.com'];
 
-const SpeedDialButton = () => {
+const GroupModal = () => {
+    
+    const [openM, setOpenM] = useState(false);
+    const handleOpenM = () => setOpenM(true);
+    const handleCloseM = () => setOpenM(false);
 
-    // speed dial
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-
-  // create-group modal
-  const [openM, setOpenM] = useState(false);
-  const handleOpenM = () => setOpenM(true);
-  const handleCloseM = () => setOpenM(false);
-
-  const addHandler = (name) => {
-    if (name === "Create Group") {
-      handleOpenM()
-    }
-    if (name === "Review") {
-      window.location.href = '/blogs'
-    }
-    if (name === "Contact") {
-      window.location.href = '/contactus'
-    }
-    if (name === "Help") {
-      window.location.href = '/help'
-    }
-  }
     return ( 
-        <div>
-          <Box sx={{ height: 300, transform: 'translateZ(0px)', flexGrow: 1, 
-           position: 'fixed', bottom: 20, right: 30, zIndex: 10  }}>
-            <SpeedDial
-              ariaLabel="SpeedDial"
-              icon={<SpeedDialIcon />}
-              onClose={handleClose}
-              onOpen={handleOpen}
-              open={open}
-            >
-            {actions.map((action) => (
-              <SpeedDialAction
-                key={action.name}
-                icon={action.icon}
-                tooltipTitle={action.name}
-                onClick={() => addHandler(action.name)}
-              />
-            ))}
-            </SpeedDial>
-          </Box>
-          
-          {/*Group-Modal*/}
-          <Dialog
+        <Dialog
             aria-labelledby="dialog-title"
             aria-describedby="dialog-description"
             onClose={handleCloseM}
@@ -132,9 +74,8 @@ const SpeedDialButton = () => {
               <Button onClick={handleCloseM}>Cancel</Button>
               <Button onClick={handleCloseM} autoFocus>Done</Button>
             </DialogActions>
-          </Dialog>
-        </div>
+        </Dialog>
      );
 }
  
-export default SpeedDialButton;
+export default GroupModal;
