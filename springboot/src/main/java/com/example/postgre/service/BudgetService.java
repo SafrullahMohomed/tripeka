a3c6aa6f32e98768a55480ba86ceb1cba9779da8
msmohamed <emails3msm@gmail.com>
2022-08-25 14:19:32 +0530

    committed budget api

--- springboot/src/main/java/com/example/postgre/service/BudgetService.java
+++ springboot/src/main/java/com/example/postgre/service/BudgetService.java
@@ -1,12 +1,13 @@
-package com.example.postgre.Service;
+package com.example.postgre.service;
 
 import com.example.postgre.Model.Budget;
 import com.example.postgre.repository.BudgetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BudgetService {
@@ -17,7 +18,41 @@ public class BudgetService {
         this.budgetRepository = budgetRepository;
     }
 
+//    to get all data from the budget table
     public List<Budget> getBudgets(){
         return budgetRepository.findAll();
     }
+
+
+////    to get the users from a particular group
+    public List<Budget> getBudgetForAGroup(Integer group_id){
+        return budgetRepository.findAllByGroupIds(group_id);
+    }
+//
+    public void addBudgetTo(Integer group_id, Integer user_id, Budget budget) {
+        budget.setGroup_id(group_id);
+        budget.setUser_id(user_id);
+        budgetRepository.save(budget);
+    }
+
+    public Double getTotalAmount(Integer group_id) {
+        return budgetRepository.getTotalAmount(group_id);
+    }
+
+    public Double getAverageAmount(Integer group_id) {
+        return budgetRepository.getAverageAmount(group_id);
+    }
+
+//    get individual amount for a user
+    public Double getIndividualTotalAmount(Integer group_id, Integer user_id){
+        return budgetRepository.getIndividualTotalAmount(group_id, user_id);
+    }
+
+//    get a budge
+    public List<Budget> getBudgetForAUser(Integer group_id, Integer user_id) {
+        return budgetRepository.findAllByGroupIdAndUserId(group_id, user_id);
+    }
+
+
 }
+
