7343329de89f0476e5a9454545358c9f2192b106
msmohamed <emails3msm@gmail.com>
2022-08-25 10:06:29 +0530

    edited form part and in login register animation part

--- react/src/pages/Login.js
+++ react/src/pages/Login.js
@@ -91,16 +91,12 @@ const Login = () => {
           <div className="px-6 h-full text-gray-800">
             <div className="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6">
               <div
-                className="flex justify-center grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-30 mt-39 md:mb-0"
-                data-aos="fade-right"
-              >
+                className="flex justify-center grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-30 mt-39 md:mb-0">
                 <img src={pop6} className="w-4/6" alt="Sample image" />
 
               </div>
               <div
-                className="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0"
-                data-aos="fade-left"
-              >
+                className="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
                 <form onSubmit={handleSubmit(onSubmit)}>
                   <div className="flex flex-row items-center justify-center lg:justify-start">
                     <p className="text-3xl mb-0 ml-auto mr-auto font-bold">
