6a97392f2bc18d234f20ff306a04cbd85e0fb2ec
AbdulQadir98 <abdul.qadirv98@gmail.com>
2022-08-29 01:19:45 +0530

    groups completed

--- react/src/components/Groups.js
+++ react/src/components/Groups.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Box from "@mui/material/Box";
+import CircularProgress from '@mui/material/CircularProgress';
 import TextField from "@mui/material/TextField";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
@@ -8,41 +9,39 @@ import Typography from "@mui/material/Typography";
 import AddCircle from "@mui/icons-material/AddCircle";
 import { grey } from "@mui/material/colors";
 import { CardActionArea } from "@mui/material";
-import { getGroups } from "../services/GroupsService";
-
-import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemAvatar from "@mui/material/ListItemAvatar";
-import ListItemText from "@mui/material/ListItemText";
-import PersonIcon from "@mui/icons-material/Person";
-import { blue } from "@mui/material/colors";
 
 import img1 from "../assets/arugam.jpg";
 import img2 from "../assets/dalada.jpg";
 import img3 from "../assets/jaffna.jpg";
 import img from "../assets/customer2.jpg";
+import { getGroups } from "../services/GroupsService";
 import createGroup from "../services/GroupsService";
 
 
 const Groups = () => {
   // Display Groups
   const [groups, setGroups] = useState([]);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   const init = () => {
     getGroups()
       .then((response) => {
-        console.log("Printing employees data", response.data);
+        console.log("Printing Groups data", response.data);
+        setIsPending(false);
         setGroups(response.data);
+        setError(null);
       })
-      .catch((error) => {
-        console.log("Something went wrong", error);
+      .catch((err) => {
+        console.log("Something went wrong", err);
+        setIsPending(false);
+        setError(err.message);
       });
   };
 
@@ -50,20 +49,25 @@ const Groups = () => {
     init();
   }, []);
 
+  // create group modal
   const [openM, setOpenM] = useState(false);
   const handleOpenM = () => setOpenM(true);
   const handleCloseM = () => setOpenM(false);
 
-  // creating croup cards
+  // creating-group form
   const [name, setName] = useState("");
   const [location, setLocation] = useState("");
 
-  const createGroupFrom = () => {
-    createGroup(name, location).then((response) => console.log(response));
+  const createGroupFrom = (e) => {
+    e.preventDefault();
+    // const group = {name, location}; console.log(group);
+    createGroup(name, location)
+      .then((response) => console.log(response));
   };
 
   return (
     <section class="text-gray-600 body-font mb-10">
+
       {/*<div>
         {groups.map((group) => (
           <div key={group.id}>
@@ -72,79 +76,49 @@ const Groups = () => {
         ))}
         </div>*/}
 
+      {/*Displaying Group Cards */}
       <div class="container px-5 py-5 mx-auto">
         <div class="w-full mb-8 pl-2">Your Trip Groups</div>
           <div class="flex flex-wrap -m-2">
-          {groups.map((group) => (
-            <div key={group.group_id} class="p-4 lg:w-1/5 md:w-1/2 w-full">
-              <Card sx={{ maxWidth: 345 }}>
-                <CardActionArea
-                  onClick={() => {
-                    window.location.href = "/trip";
-                  }}
-                >
-                  <CardMedia
-                    component="img"
-                    image={group.name === "Dalanda Palace" ? img2 : img3}
-                    alt=""
-                    sx={{ height: 100 }}
-                  />
-                  <CardContent>
-                    <Typography gutterBottom variant="h6" component="div">
-                      {group.name}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                      Created by : {group.location}
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
-            </div>
-          ))}
 
-          {/*
-          <div class="p-4 lg:w-1/5 md:w-1/2 w-full">
-            <Card sx={{ maxWidth: 345 }}>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  height="140"
-                  image={img1}
-                  alt="green iguana"
-                  sx={{ height: 100 }}
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h6" component="div">
-                    Galle Fort
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    Created by : Nishal
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </div>
-          <div class="p-4 lg:w-1/5 md:w-1/2 w-full">
-            <Card sx={{ maxWidth: 345 }}>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  height="140"
-                  image={img3}
-                  alt="green iguana"
-                  sx={{ height: 100 }}
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h6" component="div">
-                    Jaffna Trip
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    Created by : Ninthu
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </div>*/}
+            { error && 
+            <div className="flex items-center px-10 text-rose-500">
+               { error }
+            </div>
+           }
+            { isPending && 
+              <div className="flex items-center px-10">
+                  <CircularProgress />
+              </div> 
+            }
+            { groups.map((group) => (
+              <div key={group.group_id} class="p-4 lg:w-1/5 md:w-1/2 w-full">
+                <Card sx={{ maxWidth: 345 }}>
+                  <CardActionArea
+                    onClick={() => {
+                      window.location.href = "/trip";
+                    }}
+                  >
+                    <CardMedia
+                      component="img"
+                      image={group.name === "Dalanda Palace" ? img2 : img3}
+                      alt=""
+                      sx={{ height: 100 }}
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h6" component="div">
+                        {group.name}
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        Created by : {group.location}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              </div>
+            )) }
+
+          {/* Add Group Card-Button */}
           <div class="p-4 lg:w-1/5 md:w-1/2 w-full">
             <Card sx={{ maxWidth: 345 }} onClick={handleOpenM}>
               <CardActionArea>
