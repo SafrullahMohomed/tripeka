c4877f018992839bbb2ca4c38fc014b57c8d4228
msmohamed <emails3msm@gmail.com>
2022-09-13 19:17:40 +0530

    car fields for front-ed

--- react/src/components/car/CarForm.jsx
+++ react/src/components/car/CarForm.jsx
@@ -1,109 +1,185 @@
-import React from 'react';
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+// import Fab from "@mui/material/Fab";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+// import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import jwt_decode from "jwt-decode";
 
+const CarForm = () => {
+  // get current users userId
+  var currentUserId = jwt_decode(
+    JSON.parse(localStorage.getItem("user")).jwtToken
+  );
+  console.log(currentUserId.sub);
 
-const CarForm =  () =>{
-     
-        const {
-          register,
-          handleSubmit,
-          formState: { errors },
-        } = useForm();
-        const onSubmit = (data) => console.log(data);
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+  const [pickup, setPickup] = useState("");
+  const [dropoff, setDropoff] = useState("");
+  const [passengers, setPassengers] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [car_id, setCar_id] = useState("");
+  // const [user_id, setUser_id] = useState("");
+  const car_id_int = 1;
+
+  // const group_id_int = parseInt(props.group_id);
+  const user_id_int = parseInt(currentUserId.sub);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  const carbookingForm = (e) => {
+    e.preventDefault();
+
+    const carbooking = {
+      pickup,
+      dropoff,
+      users: { user_id: user_id_int },
+      car: { car_id: car_id_int },
+      passengers,
+      date,
+      time,
+      email,
+      phone,
+    };
+    console.log(carbooking);
+    // addCarbooking(budget)
+    //   .then((response) => {
+    //     console.log("Printing car booking data", response.data);
+    //     init();
+    //   })
+    //   .catch((err) => {
+    //     console.log("Something went wrong", err);
+    //   });
+  };
+
+  return (
+    <form onSubmit={carbookingForm}>
+      <DialogTitle id="expense-title" sx={{ width: 450, marginBottom: -1 }}>
       
-        
-      return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-      
-          <div className="flex flex-row items-center justify-center">
-                  <h2 class="text-emerald-700 text-3xl font-semibold my-4">
-                    Register
-                  </h2>
-                </div>
-                <div class="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5"></div>
-                
-                <div class="mb-6">
-                  <input
-                    type="text"
-                    name="firstname"
-                    class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-emerald-600 focus:outline-none"
-                    id="exampleFormControlInput4"
-                    {...register("firstname", { required: true })}
-                    placeholder="First Name"
-                  />
-                  <error className="text-red-600">
-                    {errors.username?.type === "required" &&
-                      "First Name is required"}
-                  </error>
-                </div>
+      </DialogTitle>
+      <DialogContent>
+        <DialogContentText
+          id="pickup"
+          sx={{ marginTop: 2 }}
+        ></DialogContentText>
+        <TextField
+          onChange={(e) => setPickup(e.target.value)}
+          autoFocus
+          margin="dense"
+          id="pickup"
+          label="Pickup Location"
+          type="text"
+          fullWidth
+          // variant="standard"
+        />
 
-                <div class="mb-6">
-                    <input
-                      type="text"
-                      name="lastname"
-                      class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-emerald-600 focus:outline-none"
-                      id="exampleFormControlInput3"
-                      {...register("lastname", { required: true })}
-                      placeholder="Last name"
-                    />
-                    <error className="text-red-600">
-                      {errors.username?.type === "required" &&
-                        "Last Name is required"}
-                    </error>
-                  </div>
+        <DialogContentText
+          id="dropoff"
+          sx={{ marginTop: 2 }}
+        ></DialogContentText>
+        <TextField
+          onChange={(e) => setDropoff(e.target.value)}
+          autoFocus
+          margin="dense"
+          id="dropoff"
+          label="Dropoff Location"
+          type="text"
+          fullWidth
+          // variant="standard"
+        />
 
-                  <div class="mb-6">
-                    <input
-                      type="text"
-                      name="username"
-                      class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-emerald-600 focus:outline-none"
-                      id="exampleFormControlInput2"
-                      {...register("username", { required: true })}
-                      placeholder="Username"
-                    />
-                    <error className="text-red-600">
-                      {errors.username?.type === "required" &&
-                        "Name is required"}
-                    </error>
-                  </div>
+        <DialogContentText id="date" sx={{ marginTop: 2 }}></DialogContentText>
+        <TextField
+          onChange={(e) => setDate(e.target.value)}
+          autoFocus
+          margin="dense"
+          id="date"
+          label="Date"
+          type="date"
+          fullWidth
+          focused
+          // variant="standard"
+        />
 
-                  <div class="mb-6">
-                    <input
-                      type="text"
-                      name="email"
-                      class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-emerald-600 focus:outline-none"
-                      id="exampleFormControlInput2"
-                      placeholder="Email address"
-                      {...register("email", {
-                        required: true,
-                        pattern:
-                          /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/i,
-                      })}
-                    />
-                    <error className="text-red-600">
-                      {errors.email?.type === "required" && "Email is required"}
-                      {errors.email?.type === "pattern" &&
-                        "Entered email is in wrong format"}
-                    </error>
-                  </div>
+        <DialogContentText id="time" sx={{ marginTop: 2 }}></DialogContentText>
+        <TextField
+          onChange={(e) => setTime(e.target.value)}
+          autoFocus
+          margin="dense"
+          id="time"
+          label="Time"
+          type="time"
+          fullWidth
+          focused
+          // variant="standard"
+        />
 
-                  <div class="mb-6">
-                    <select 
-                      {...register("role")}
-                      id="role"
-                      name="role"
-                      class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-emerald-600 focus:outline-none"
-                    >
-                      <option selected>Choose your type</option>
-                      <option value="TRAVELLER">Traveller</option>
-                      <option value="LODGE_PROVIDER">Lodge Provider</option>
-                      <option value="VEHICLE_OWNER">Vehicle Owner</option>
-                      <option value="TRIP_GUIDER">Trip Guider</option>
-                    </select>
-                  </div>
-        </form>
-      );
-      
-}
+        <DialogContentText
+          id="passengers"
+          sx={{ marginTop: 2 }}
+        ></DialogContentText>
+        <TextField
+          onChange={(e) => setPassengers(e.target.value)}
+          autoFocus
+          margin="dense"
+          id="passengers"
+          label="Number of Passengers"
+          type="number"
+          fullWidth
+          // variant="standard"
+        />
+
+        <DialogContentText id="email" sx={{ marginTop: 2 }}></DialogContentText>
+        <TextField
+          onChange={(e) => setPassengers(e.target.value)}
+          autoFocus
+          margin="dense"
+          id="email"
+          label="Email"
+          type="email"
+          fullWidth
+          // variant="standard"
+        />
+
+        <DialogContentText id="phone" sx={{ marginTop: 2 }}></DialogContentText>
+        <TextField
+          onChange={(e) => setPassengers(e.target.value)}
+          autoFocus
+          margin="dense"
+          id="phone"
+          label="Mobile Number"
+          type="tel"
+          fullWidth
+          // variant="standard"
+        />
+      </DialogContent>
+      <DialogActions>
+        {/* <Button onClick={handleClose}>Cancel</Button> */}
+
+        <div class="flex space-x-2 justify-center ">
+          <button
+            type="button"
+            data-mdb-ripple="true"
+            data-mdb-ripple-color="light"
+            class="inline-block px-6 py-2.5 bg-green-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-green-700 hover:shadow-lg  focus:ring-0  active:shadow-lg transition duration-150 ease-in-out"
+          >
+            Book
+          </button>
+        </div>
+      </DialogActions>
+    </form>
+  );
+};
 
 export default CarForm;
--- react/src/components/car/CarMain.jsx
+++ react/src/components/car/CarMain.jsx
@@ -1,28 +1,35 @@
 import React from "react";
 import Header from "../Header";
 import CarForm from "./CarForm";
+import CarImage from "./CarImage";
+import Footer from "../Footer";
 
 const CarMain = () => {
   return (
     <div>
-       {/* header component */}
 
+       {/* header component */}
       <div className="class-header">
         <Header />
-      </div>
-         
+      </div>   
 
        {/* main component */}
-      <div className="class-main">
+      <div className="class-main flex justify-between">
+        <div className="car-left w-full item-center flex flex-col ">
+          <div className="left-title text-emerald-700 2xl:text-5xl font-semibold my-4 flex justify-center mt-12 md:text-3xl lg:text-4xl"> BOOK A CAR</div>
+          <CarImage />
+        </div>
 
         {/* car form component */}
-        <div className="class-car-form">
-            <CarForm />
+        <div className="car-right w-full">
+            <CarForm className="w-full" />
         </div>
       </div>
 
          {/* footer component */}
-        <div className="class-footer"></div>
+        <div className="class-footer">
+          <Footer />
+        </div>
 
     </div>
   );
