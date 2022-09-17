0cd3333ee32c195b54be1be21a0aa33a6910ad35
Ashfaq Ashar <72542210+ashfaq1998-data@users.noreply.github.com>
2022-09-17 21:57:11 +0530

    Hotel front page done

---
+++ react/src/components/TripFavourite/TripFavourite.js
@@ -0,0 +1,66 @@
+import React from 'react'
+
+const TripFavourite = () => {
+    return (
+        <div className="fp" style={{width: "100%",maxWidth: "1024px", display: "flex",justifyContent: "space-between",gap: "20px"}}>
+          <div className="fpItem" style={{flex: "1", gap: "10px", display: "flex",flexDirection: "column"}}>
+            <img
+              src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
+              alt=""
+              style={{ width: "100%",height: "250px",objectFit: "cover"}}
+            />
+            <span className="fpName" style={{color: "#333",fontWeight: "bold"}}>Taj Samudra</span>
+            <span className="fpCity" style={{fontWeight: "300"}}>Colombo</span>
+            <span className="fpPrice" style={{fontWeight: "500"}}>Starting from LKR 1200</span>
+            <div className="fpRating">
+              <button style={{backgroundColor: "#003580",color: "white",border: "none",padding: "3px",marginRight: "10px",fontWeight: "bold"}}>8.9</button>
+              <span style={{fontSize: "14px"}}>Excellent</span>
+            </div>
+          </div>
+          <div className="fpItem" style={{flex: "1", gap: "10px", display: "flex",flexDirection: "column"}}>
+            <img
+              src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
+              alt=""
+              style={{ width: "100%",height: "250px",objectFit: "cover"}}
+            />
+            <span className="fpName" style={{color: "#333",fontWeight: "bold"}}>Taj Samudra</span>
+            <span className="fpCity" style={{fontWeight: "300"}}>Colombo</span>
+            <span className="fpPrice" style={{fontWeight: "500"}}>Starting from LKR 1200</span>
+            <div className="fpRating">
+              <button style={{backgroundColor: "#003580",color: "white",border: "none",padding: "3px",marginRight: "10px",fontWeight: "bold"}}>8.9</button>
+              <span style={{fontSize: "14px"}}>Excellent</span>
+            </div>
+          </div>
+          <div className="fpItem" style={{flex: "1", gap: "10px", display: "flex",flexDirection: "column"}}>
+            <img
+              src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
+              alt=""
+              style={{ width: "100%",height: "250px",objectFit: "cover"}}
+            />
+            <span className="fpName" style={{color: "#333",fontWeight: "bold"}}>Taj Samudra</span>
+            <span className="fpCity" style={{fontWeight: "300"}}>Colombo</span>
+            <span className="fpPrice" style={{fontWeight: "500"}}>Starting from LKR 1200</span>
+            <div className="fpRating">
+              <button style={{backgroundColor: "#003580",color: "white",border: "none",padding: "3px",marginRight: "10px",fontWeight: "bold"}}>8.9</button>
+              <span style={{fontSize: "14px"}}>Excellent</span>
+            </div>
+          </div>
+          <div className="fpItem" style={{flex: "1", gap: "10px", display: "flex",flexDirection: "column"}}>
+            <img
+              src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1"
+              alt=""
+              style={{ width: "100%",height: "250px",objectFit: "cover"}}
+            />
+            <span className="fpName" style={{color: "#333",fontWeight: "bold"}}>Taj Samudra</span>
+            <span className="fpCity" style={{fontWeight: "300"}}>Colombo</span>
+            <span className="fpPrice" style={{fontWeight: "500"}}>Starting from LKR 1200</span>
+            <div className="fpRating">
+              <button style={{backgroundColor: "#003580",color: "white",border: "none",padding: "3px",marginRight: "10px",fontWeight: "bold"}}>8.9</button>
+              <span style={{fontSize: "14px"}}>Excellent</span>
+            </div>
+          </div>
+        </div>
+      );
+}
+
+export default TripFavourite
