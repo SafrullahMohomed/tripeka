6f9486f0b9734d34911a7abdf796c3047139e2a3
AbdulQadir98 <abdul.qadirv98@gmail.com>
2022-09-17 08:58:50 +0530

    parsing userId

--- react/src/components/NotFound.js
+++ react/src/components/NotFound.js
@@ -1,6 +1,11 @@
 import { Link } from "react-router-dom"
 import img from '../assets/pageNotFound.png'
 
+var user_id = null;
+if (localStorage.getItem("userDetails")) {
+  user_id = JSON.parse(localStorage.getItem("userDetails")).user_id;
+}
+
 const NotFound = () => {
     return ( 
         <div className="flex flex-col items-center mb-20 w-full">
@@ -9,7 +14,7 @@ const NotFound = () => {
                 <img className="h-96" src={img} alt="" />
             </div>
             <div className="p-2 bg-emerald-200 text-xl rounded-lg">
-                <Link to="/dashboard">Back to the homepage</Link>
+                <Link to={`/dashboard/${user_id}`}>Back to the homepage</Link>
             </div>
             
         </div>
