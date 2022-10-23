865ac88fc18ee59955f8e5254d453dd832409946
msmohamed <emails3msm@gmail.com>
2022-10-23 14:43:42 +0530

    created the car card list for user to get the filtered vehicles

--- react/src/components/car/carListing/CarCardInnerDetails.jsx
+++ react/src/components/car/carListing/CarCardInnerDetails.jsx
@@ -1,40 +1,28 @@
 import React from "react";
 import { useState, useEffect } from "react";
 
-const CarCardInnerDetails = () => {
+const CarCardInnerDetails = (props) => {
   const [carList, setCarList] = useState([]);
 
-  useEffect((props) => {
-    fetch("http://localhost:3000/cars")
-      .then((res) => res.json())
-      .then((data) => {
-        setCarList(data);
-      });
-  }, []);
+  useEffect(() => {}, []);
 
   return (
     <div>
-      <div className="carStatus">{props.carStatus}carStatus</div>
-      <div className="carType">{props.carType}carType</div>
-      <div className="carOwner">{props.carOwner}carOwner</div>
-      <div className="carPhone">{props.carPhone}carPhone</div>
+      <div className=" flex flex-col items-start ">
+        {/* <div className="carStatus">{props.carStatus}carStatus</div> */}
+        <div className="carType">Vehicle Type : {props.carType}</div>
+        <div className="carOwner">Vehicle Owner : {props.carOwner}</div>
+        <div className="carPhone">Phone NO : {props.carPhone}</div>
 
-      <div className="carTotalprice">{props.carTotalprice}carTotalprice</div>
-      <div className="carAvailableSeats">
-        {props.carAvailableSeats}carAvailableSeats
-      </div>
-      <div class="flex space-x-2 justify-center mr-10 pb-10 ">
-        <button
-          type="button"
-          data-mdb-ripple="true"
-          data-mdb-ripple-color="light"
-          class="inline-block px-10 py-2.5 bg-green-600 text-white font-medium text-lg leading-tight uppercase
-           rounded shadow-md hover:bg-green-700 hover:shadow-lg  focus:ring-0  active:shadow-lg transition 
-           duration-150 ease-in-out"
-        >
-          Book Now
-        </button>
-      </div>
+        <div className="carTotalprice">
+          Total price : Rs. {props.carTotalprice}
+        </div>
+        <div className="carAvailableSeats">
+          Available No Seats : {props.carAvailableSeats}
+        </div>
+      </div>      
     </div>
   );
 };
+
+export default CarCardInnerDetails;
--- react/src/components/car/carListing/CarListingMain.jsx
+++ react/src/components/car/carListing/CarListingMain.jsx
@@ -1,37 +1,138 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import Header from "../../Header";
+import CarCard from "./CarCard";
 
 const CarListingMain = () => {
   const [carList, setCarList] = useState([]);
 
+  const data = [
+    {
+      carId: 1,
+      carName: "Volvo",
+      carType: "Car",
+      carOwner: "John",
+      carPhone: "123456789",
+      carStatus: "Available",
+      carTotalprice: 1000.0,
+      carAvailableSeats: 4,
+      carPrice: 120.0,
+      carImage: "https://picsum.photos/seed/picsum/200/300",
+    },
+    {
+      carId: 2,
+      carName: "BMW",
+      carType: "Car",
+      carOwner: "John",
+      carPhone: "123456789",
+      carStatus: "Available",
+      carTotalprice: 1000.0,
+      carAvailableSeats: 4,
+      carPrice: 120.0,
+      carImage: "https://picsum.photos/seed/picsum/200/300",
+    },
+    {
+      carId: 3,
+      carName: "Audi",
+      carType: "Car",
+      carOwner: "John",
+      carPhone: "123456789",
+      carStatus: "Available",
+      carTotalprice: 1000.0,
+      carAvailableSeats: 4,
+      carPrice: 120.0,
+      carImage: "https://picsum.photos/seed/picsum/200/300",
+    },
+    {
+      carId: 4,
+      carName: "Mercedes",
+      carType: "Car",
+      carOwner: "John",
+      carPhone: "123456789",
+      carStatus: "Available",
+      carTotalprice: 1000.0,
+      carAvailableSeats: 4,
+      carPrice: 120.0,
+      carImage: "https://picsum.photos/seed/picsum/200/300",
+    },
+    {
+      carId: 5,
+      carName: "Toyota",
+      carType: "Car",
+      carOwner: "John",
+      carPhone: "123456789",
+      carStatus: true,
+      carTotalprice: 1000.0,
+      carAvailableSeats: 4,
+      carPrice: 120.0,
+      carImage: "https://picsum.photos/seed/picsum/200/300",
+    },
+    {
+      carId: 6,
+      carName: "Honda",
+      carType: "Car",
+      carOwner: "John",
+      carPhone: "123456789",
+      carStatus: "Available",
+      carTotalprice: 1000.0,
+      carAvailableSeats: 4,
+      carPrice: 120.0,
+      carImage: "https://picsum.photos/seed/picsum/200/300",
+    },
+    {
+      carId: 7,
+      carName: "Suzuki",
+      carType: "Car",
+      carOwner: "John",
+      carPhone: "123456789",
+      carStatus: "Available",
+      carTotalprice: 1000.0,
+      carAvailableSeats: 4,
+      carPrice: 120.0,
+      carImage: "https://picsum.photos/seed/picsum/200/300",
+    },
+  ];
+
+  // console.log(data);
+  // console.log(carList);
+
   useEffect(() => {
-    fetch("http://localhost:3000/cars")
-      .then((res) => res.json())
-      .then((data) => {
-        setCarList(data);
-      });
-  }, []);
+    //   fetch("http://localhost:3000/cars")
+    //     .then((res) => res.json())
+    //     .then((data) => {
+    //       setCarList(data);
+    //     });
+    setCarList(data);
+  }, [JSON.stringify(data)]);
 
   return (
     <div>
+      {/* adding the header for car */}
+      <Header />
       {/* to get the grid card view for the card list */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 grid-center mx-auto ">
         {carList.map((car) => {
-          return;
-          <CarCard
-            key={car.carId}
-            carName={car.carName}
-            carOwner={car.carOwner}
-            carPhone={car.carPhone}
-            carType={car.carType}
-            carPrice={car.carPrice}
-            carImage={car.carImage}
-            carStatus={car.carstatus}
-            carTotalprice={car.carTotalprice}
-            carAvailableSeats={car.carAvailableSeats}
-          />;
+
+          return (
+            
+            
+            <CarCard
+              key={car.carId}
+              carName={car.carName}
+              carOwner={car.carOwner}
+              carPhone={car.carPhone}
+              carType={car.carType}
+              carPrice={car.carPrice}
+              carImage={car.carImage}
+              carStatus={car.carstatus}
+              carTotalprice={car.carTotalprice}
+              carAvailableSeats={car.carAvailableSeats}
+            />
+          );
         })}
       </div>
     </div>
   );
 };
+
+export default CarListingMain;
