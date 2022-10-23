a70209c85e44bf06cd12b3d85df5778ceaf06302
msmohamed <emails3msm@gmail.com>
2022-10-23 14:59:31 +0530

    car driver console listing

---
+++ react/src/components/car/carDriver/CarDriverInnerDetails.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+
+const CarDriverInnerdetails = (props) => {
+    return( <div>
+        <div className=" flex flex-col items-start ">
+          {/* <div className="carStatus">{props.carStatus}carStatus</div> */}
+          <div className="from">From : <span className='text-blue-600'>{props.carPickupLocation}</span></div>
+            <div className="to">To : <span className='text-blue-600'>{props.carDropLocation}</span></div>
+            <div className="numberOfPeople">Number of people <span className='text-blue-600'>{props.noOfPeople}</span></div>
+            <div className="totalPrice">ride price : <span className='text-blue-600'>{props.carTotalPrice}</span></div>
+            <div className="userPhone">Phone NO : <span className='text-blue-600'>{props.carRequestedUserPhone}</span></div>
+        </div>
+        <br />
+      </div>);
+}
+
+export default CarDriverInnerdetails;
