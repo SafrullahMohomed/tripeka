26cd5942129b45f46d8646252c865e68f2bdc8f4
Dinujaya <kularathnadinujaya@gmail.com>
2022-10-26 18:16:16 +0530

    notiftest

---
+++ react/src/components/notifitest12.js
@@ -0,0 +1,23 @@
+import React  from "react";
+import "./notifitest.css";
+
+
+function notifitest(){
+
+
+
+    return(
+        <div className="container">
+            <h1>React Notification</h1>
+
+            <ReacNotifi/>
+            <Home/>
+         
+        </div>
+    );
+
+}
+
+
+
+export default notifitest;
