88a508200481ba681ee51336aa8bc0753870f065
shanika-96 <87492020+shanika-96@users.noreply.github.com>
2022-10-26 13:28:52 +0530

    Search user changed

--- react/src/components/SearchUser.js
+++ react/src/components/SearchUser.js
@@ -3,7 +3,16 @@ const breakpoints = [1080, 640, 384, 256, 128, 96, 64, 48];
 const unsplashLink = (id, width, height) =>
     `https://source.unsplash.com/${id}/${width}x${height}`;
 
+    export default Profile;
 
+    import React from 'react'
+    import Footer from '../components/Footer'
+    import Navbar from '../components/Navbar'
+    import pop6 from "../assets/unlock.png";
+    import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+    import {
+        faPencil
+      }
 
 const photos = unsplashPhotos.map((photo) => ({
     src: unsplashLink(photo.id, photo.width, photo.height),
