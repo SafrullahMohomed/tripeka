2e2dca0cf85cb22f74a7197a9e44a9b91a9fb215
Dinujaya <kularathnadinujaya@gmail.com>
2022-10-27 01:38:48 +0530

    Testblog3fix

--- react/src/components/TestBlog1.js
+++ react/src/components/TestBlog1.js
@@ -29,39 +29,39 @@ const TestBlog1= ()=> {
    
     
 // }
-// const [accept, setaccept] = useEffect(true);
+const [accept, setaccept] = useEffect(true);
 
 
-    // const [modal, setModal] = useState(false);
+    const [modal, setModal] = useState(false);
   
-    // const toggleModal = () => {
-    //   setModal(!modal);
-    // };
+    const toggleModal = () => {
+      setModal(!modal);
+    };
   
-    // if(modal) {
-    //   document.body.classList.add('active-modal')
-    // } else {
-    //   document.body.classList.remove('active-modal')
+    if(modal) {
+      document.body.classList.add('active-modal')
+    } else {
+      document.body.classList.remove('active-modal')
     
-    //   {modal && (
-    //     <div className="modal">
-    //       <div  className="overlay"></div>
-    //       <div className="modal-content">
-    //         <h2>Hello Modal</h2>
-    //         <p>
-    //           Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
-    //           perferendis suscipit officia recusandae, eveniet quaerat assumenda
-    //           id fugit, dignissimos maxime non natus placeat illo iusto!
-    //           Sapiente dolorum id maiores dolores? Illum pariatur possimus
-    //           quaerat ipsum quos molestiae rem aspernatur dicta tenetur. Sunt
-    //           placeat tempora vitae enim incidunt porro fuga ea.
-    //         </p>
-    //         <button className="close-modal" onClick={toggleModal}>
-    //           CLOSE
-    //         </button>
-    //       </div>
-    //     </div>
-    //   )}
+      {modal && (
+        <div className="modal">
+          <div  className="overlay"></div>
+          <div className="modal-content">
+            <h2>Hello Modal</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+              perferendis suscipit officia recusandae, eveniet quaerat assumenda
+              id fugit, dignissimos maxime non natus placeat illo iusto!
+              Sapiente dolorum id maiores dolores? Illum pariatur possimus
+              quaerat ipsum quos molestiae rem aspernatur dicta tenetur. Sunt
+              placeat tempora vitae enim incidunt porro fuga ea.
+            </p>
+            <button className="close-modal" onClick={toggleModal}>
+              CLOSE
+            </button>
+          </div>
+        </div>
+      )}
 
         const settings = {
             dots: false,
@@ -144,5 +144,6 @@ const TestBlog1= ()=> {
         </div>
       );
     }
+}
 
   export default TestBlog1; 
