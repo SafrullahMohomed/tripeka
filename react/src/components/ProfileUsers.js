62cef8fc3b814fc159f4cb9e483d4dd79ed03ef5
shanika-96 <87492020+shanika-96@users.noreply.github.com>
2022-10-26 12:54:12 +0530

    Profile users added

---
+++ react/src/components/ProfileUsers.js
@@ -0,0 +1,16 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Profile = () => {
+    return ( 
+    <>
+        <Header />
+        <section class="text-gray-600 body-font mb-10 px-32 py-5">
+            Profile Page
+        </section>
+        <Footer />
+    </>
+    );
+}
+ 
+export default Profile;
