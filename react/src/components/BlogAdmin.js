db44fb881a43ce9e2d6c3a6246d4145d4592fc07
AbdulQadir98 <abdul.qadirv98@gmail.com>
2022-10-27 02:23:07 +0530

    add suggestions

--- react/src/components/BlogAdmin.js
+++ react/src/components/BlogAdmin.js
@@ -18,8 +18,10 @@ import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 
-const Review = () => {
-  const [blogs, setBlogs] = useState([]);   
+
+const BlogAdmin = () => {
+
+    const [blogs, setBlogs] = useState([]);   
 
   // Generate Random Colors on avatar
   function stringToColor(string) {
@@ -65,32 +67,14 @@ const Review = () => {
     init();
   }, []);
 
-
     return ( 
-           
-        <section className="text-gray-600 body-font pb-10 bg-gray-100">
-          <div className="pr-10 pt-10">
-            <Fab 
-                  sx={{position: 'fixed', top: 100, right: 30, zIndex: 10}} color="primary" aria-label="edit"
-                  href='/blogs'
-              >
-                <Tooltip title="Write Blog" arrow>   
-                    <EditIcon />
-                </Tooltip>
-            </Fab>
-          </div>
-            <div class="flex flex-wrap w-full pb-4 flex-col items-center text-center">
-              <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Travelers' Blogs</h1>
-              <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">Traveler Reveiws and discussion of the quality of Locations</p>
-            </div>
-            {/* <div>
-                {blogs.map((blog, index) => (
-                  <div key={index}>
-                      <div>{blog.title}</div>
-                  </div>
-                ))}
-            </div> */}
+        <section class="w-full h-full text-gray-600 body-font pt-20 bg-gray-100">
+            <div class="flex justify-center px-5 py-1 mx-auto">
+                
             <div class="container px-18 py-5 mx-auto">
+            <div class="flex flex-wrap w-full pb-4 flex-col items-center text-center">
+              <div class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Published Blogs</div>
+            </div>
                 <div className="px-16 mx-auto">
                     <div class="flex justify-center flex-wrap">
                     {blogs.map((blog, index) => (
@@ -120,8 +104,10 @@ const Review = () => {
                     </div>
                 </div>
             </div>
+
+            </div>
         </section>
      );
 }
  
-export default Review;
+export default BlogAdmin;
